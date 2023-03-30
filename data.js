7533d320744aac27bb225da28925fa4158db67c5
Kieran <kieran.webdevelopment@gmail.com>
2023-03-30 12:51:00 +0100

    added data file

---
+++ data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+  {
+    name: 'Pizza',
+    ingredients: ['pepperoni', 'mushroom', 'mozarella'],
+    price: 14,
+    emoji: '🍕',
+    id: 0,
+    image: './images/pizza.jpg',
+  },
+  {
+    name: 'Hamburger',
+    ingredients: ['beef', 'cheese', 'lettuce'],
+    price: 12,
+    emoji: '🍔',
+    id: 1,
+    image: './images/hamburger.jpg',
+  },
+  {
+    name: 'Beer',
+    ingredients: ['grain, hops, yeast, water'],
+    price: 12,
+    emoji: '🍺',
+    id: 2,
+    image: './images/beer.jpg',
+  },
+];
